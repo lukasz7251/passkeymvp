aba2814c5211f02236fc5051348725f6401f454f
Łukasz Szymańczuk <lukasz.szymanczuk@bm.pl>
2024-02-19 18:53:43 +0100

    init

---
+++ src/main/kotlin/com/example/passkeymvp/PasskeymvpApplication.kt
@@ -0,0 +1,11 @@
+package com.example.passkeymvp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PasskeymvpApplication
+
+fun main(args: Array<String>) {
+	runApplication<PasskeymvpApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/passkeymvp/PasskeymvpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.passkeymvp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PasskeymvpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
